6078e3482cef8a1b6a1f395641fea12f314bbf26
pankajjossy1 <50371645+pankajjossy1@users.noreply.github.com>
2020-11-11 12:54:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -33,10 +33,10 @@
     </div>
    <div class="collapse navbar-collapse navbar-menubuilder">
     <ul class="nav navbar-nav">
-      <li class="space"><a href="compiler.php"><i class="fa fa-code ispace"></i>Compiler</a></li>
-      <li class="space"><a href="archive.php"><i class="fa fa-archive ispace"></i>Problem Archive</a></li>
-      <li class="space"><a href="contest.php"><i class="fa fa-cogs ispace"></i>Contests</a></li>
-      <li class="space"><a href="#"><i class="fa fa-check-square ispace"></i>Debug</a></li>
+      <li class="space"><a href="compiler.php"><i class="fa fa-code ispace"></i>BCA Department</a></li>
+      <li class="space"><a href="archive.php"><i class="fa fa-archive ispace"></i></a></li>
+      <li class="space"><a href="contest.php"><i class="fa fa-cogs ispace"></i></a></li>
+      <li class="space"><a href="#"><i class="fa fa-check-square ispace"></i></a></li>
      
       
     </ul>
